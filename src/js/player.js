0a3791e71489940356a0025fe2c020562c689bed
Sebastian Kropp <sebastian@kresge-100-64-81-163.resnet.ucsc.edu>
2022-03-01 21:34:33 -0800

    Added top dropdown for selecting {today,week,year,alltime}

--- src/js/player.js
+++ src/js/player.js
@@ -1,8 +1,9 @@
 import { useLocation, useNavigate } from 'react-router-dom';
 import ReactPlayer from 'react-player/lazy'
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faForward, faBackward, faFire, faCertificate, faSignal } from '@fortawesome/free-solid-svg-icons';
+import { faComment } from '@fortawesome/free-regular-svg-icons';
 
 
 function isValidVideo(child) {
@@ -16,11 +17,11 @@ function isValidVideo(child) {
 
 function retrieveURL(child) {
   if (child.data.is_video && child.data.media.reddit_video.fallback_url != null) {
-    return child.data.media.reddit_video.fallback_url
+    return { url: child.data.media.reddit_video.fallback_url, name: child.data.name, permalink: child.data.permalink }
   }
 
   if (child.data.url != null) {
-    return child.data.url
+    return { url: child.data.url, name: child.data.name, permalink: child.data.permalink }
   }
 }
 
@@ -31,20 +32,33 @@ const Player = () => {
   const navigate = useNavigate();
   const { state } = useLocation();
   const [count, setCount] = useState(0);
-  const [posts, setPosts] = useState([]);
-  const [selector, setSelector] = useState("hot")
+  const [posts, setPosts] = useState();
+  const [selector, setSelector] = useState({ type: "hot" })
   const [isHovering, setIsHovering] = useState()
+  const [kind, setKind] = useState("");
+  const [dropDownValue, setDropDownValue] = useState("Todays")
+  const top = useRef(null);
 
   //const count = 0;
   useEffect(() => {
     const fetchData = () => {
-      fetch("https://www.reddit.com/r/" + state.sub + "/" + selector + ".json")
-        .then(res => {
-          if (res.status != 200) {
-            //console.log('error')
-          }
-          return res.json()
-        })
+      var fetchUrl;
+      switch (true) {
+        case selector.type == "top":
+          console.log("this is the time selected: ", selector.time)
+          fetchUrl = fetch("https://www.reddit.com/r/" + state.sub + "/" + selector.type + "/.json?t=" + selector.time + "&count=25&after=" + kind);
+          console.log("https://www.reddit.com/r/" + state.sub + "/" + selector.type + "/.json?t=" + selector.time + "&count=25&after=" + kind)
+          break;
+        default:
+          fetchUrl = fetch("https://www.reddit.com/r/" + state.sub + "/" + selector.type + ".json?count=25&after=" + kind);
+      }
+
+      fetchUrl.then(res => {
+        if (res.status !== 200) {
+          //console.log('error')
+        }
+        return res.json()
+      })
         .then(data => {
           console.log("json:", data.data.children);
           var urlArray = data.data.children
@@ -54,7 +68,7 @@ const Player = () => {
         })
     }
     fetchData();
-  }, [selector]);
+  }, [selector, kind]);
   //const fetchURLArray = fetchData();
 
   const DisplayOverlay = () => {
@@ -69,8 +83,35 @@ const Player = () => {
       </div>
     )
   }
+  const DropDown = () => {
+    if (top.current == null) {
+      return null;
+    }
+    console.log(top.current.className == "liSelectorActive");
+    if (top.current.className == "liSelectorActive") {
+      return (
+        <select className="dropDown"
+          name="Time"
+          value={dropDownValue}
+          onClick={(event) => event.stopPropagation()}
+          onChange={(event) => { setDropDownValue(event.target.value); setSelector({ type: "top", time: event.target.value }) }}>
+          <option value="today">Today</option>
+          <option value="week">This Week</option>
+          <option value="year">This Year</option>
+          <option value="all">All Time</option>
+        </select>
+      )
+    }
+    else {
+      return null;
+    }
+  }
+  if (count == 24) {
+    setKind(posts[count - 1].name);
+    setCount(0);
+  }
+  console.log(count);
   console.log(posts);
-
   return (
     <body>
       <ul display="inline-block">
@@ -85,17 +126,21 @@ const Player = () => {
         </li>
       </ul>
       <ul className="ulSelector">
-        <li className={isactive[0] ? "liSelectorActive" : "liSelectorInactive"} onClick={() => { setIsActive([1, 0, 0]); setCount(0); setSelector('hot') }}>
+        <li className={isactive[0] ? "liSelectorActive" : "liSelectorInactive"} onClick={() => { setIsActive([1, 0, 0]); setCount(0); setSelector({ type: 'hot' }) }}>
           <FontAwesomeIcon id="Hot" icon={faFire} />
           <label for="Hot"> Hot</label>
         </li>
-        <li className={isactive[1] ? "liSelectorActive" : "liSelectorInactive"} onClick={() => { setIsActive([0, 1, 0]); setCount(0); setSelector('new'); }}>
+        <li className={isactive[1] ? "liSelectorActive" : "liSelectorInactive"} onClick={() => { setIsActive([0, 1, 0]); setCount(0); setSelector({ type: 'new' }); }}>
           <FontAwesomeIcon id="New" icon={faCertificate} />
           <label for="New"> New</label>
         </li>
-        <li className={isactive[2] ? "liSelectorActive" : "liSelectorInactive"} onClick={() => { setIsActive([0, 0, 1]); setCount(0); setSelector('top'); }}>
+        <li ref={top} className={isactive[2] ? "liSelectorActive" : "liSelectorInactive"} onClick={() => { setIsActive([0, 0, 1]); setCount(0); setSelector({ type: 'top', time: "day" }); }}>
           <FontAwesomeIcon id="Top" icon={faSignal} />
           <label for="Top"> Top</label>
+          <DropDown padding-left="5em" />
+        </li>
+        <li className="liSelectorActive" onClick={() => window.open("https://www.reddit.com" + posts[count].permalink)}>
+          <FontAwesomeIcon id="Comments" icon={faComment} />
         </li>
       </ul>
       <div
@@ -104,7 +149,7 @@ const Player = () => {
         onMouseLeave={() => setIsHovering(false)}>
         {posts ?
           <ReactPlayer
-            url={"urls:", posts[count]}
+            url={posts[count].url}
             className='react-player'
             width="100%"
             height="100%"
